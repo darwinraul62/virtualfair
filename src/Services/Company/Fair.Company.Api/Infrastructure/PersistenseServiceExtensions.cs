3035e860631d871800aba197bee105ed29c21604
darwinraul62 <darwinraul62@gmail.com>
2022-07-07 15:55:49 -0500

    Add Entity Framework Migration for Initial Model

--- src/Services/Company/Fair.Company.Api/Infrastructure/PersistenseServiceExtensions.cs
+++ src/Services/Company/Fair.Company.Api/Infrastructure/PersistenseServiceExtensions.cs
@@ -6,13 +6,24 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class PersistenseServiceExtensions
 {
-    public static void AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
+    public static void AddPersistenceService(this IServiceCollection services, 
+        IConfiguration configuration)
     {
         services.AddDbContext<CompanyDbContext>(options =>
         {
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));            
-        });        
-
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+                b => b.MigrationsAssembly("Fair.Company.Infrastructure.SqlServer"));                        
+        });            
+        
         services.AddTransient<IPersistenceService, PersistenceService>();
     }
+
+    public static void UseDataBaseUpdate(this WebApplication app)
+    {
+        using (var serviceScope = app.Services.CreateScope())
+        {
+            var context = serviceScope.ServiceProvider.GetRequiredService<CompanyDbContext>();
+            context.Database.Migrate();
+        }
+    }    
 }
---
+++ src/Services/Company/Fair.Company.Infrastructure.SqlServer/Migrations/20220707025919_CreateCompany.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fair.Company.Infrastructure.SqlServer.Migrations
+{
+    public partial class CreateCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Identification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.CompanyId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Company");
+        }
+    }
+}
