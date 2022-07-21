c1f17bd503eab644106fbb03bcc8c7970e364c6c
darwinraul62 <darwinraul62@gmail.com>
2022-07-21 18:27:15 -0500

    Fix InitializationService, Resolve CompanyDbContext From Scope ServiceProvider

--- src/Services/Company/Fair.Company.Api/HostedServices/InitializeDataWorker.cs
+++ src/Services/Company/Fair.Company.Api/HostedServices/InitializeDataWorker.cs
@@ -1,25 +1,24 @@
 using Fair.Company.Api.Services;
 using Fair.Company.Data;
+using Fair.Company.Infrastructure;
 
 namespace Fair.Company.Api.HostedServices;
 
 public class InitializeDataWorker : IHostedService
 {    
-    private readonly IServiceProvider serviceProvider;
-
+    private readonly IServiceProvider serviceProvider;    
     public InitializeDataWorker(IServiceProvider serviceProvider)
-    {
+    {        
         ArgumentNullException.ThrowIfNull(serviceProvider);
-        this.serviceProvider = serviceProvider;
+        this.serviceProvider = serviceProvider;      
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
-    {        
-        var initializationService = serviceProvider.GetService<InitializationService>();
+    {
+        using var scope = serviceProvider.CreateScope();
 
-        if(initializationService == null)
-            throw new InvalidOperationException("InitializationService service not found");        
-
+        var initializationService = scope.ServiceProvider.CreateScope().ServiceProvider.GetRequiredService<InitializationService>();
+        
         await initializationService.CreateHostCompanyIfNotExistsAsync();
     }
     
